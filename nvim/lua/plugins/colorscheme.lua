997a91f58f9ce64190b1eb85baadcd7f8825517b
Andres Ruiz <asrj17@gmail.com>
2024-11-07 10:43:08 +0800

    Add nvim setup

---
+++ nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,32 @@
+return {
+  -- add dracula
+  -- {
+  -- "Mofiqul/dracula.nvim",
+  -- opts = {
+  -- transparent = true,
+  -- styles = {
+  -- sidebars = "transparent",
+  -- floats = "transparent",
+  -- },
+  -- },
+  -- },
+
+  -- Configure LazyVim to load dracula
+  -- {
+  -- "LazyVim/LazyVim",
+  -- opts = {
+  -- colorscheme = "dracula",
+  -- },
+  -- },
+  {
+    "folke/tokyonight.nvim",
+
+    opts = {
+      transparent = true,
+      styles = {
+        sidebars = "transparent",
+        floats = "transparent",
+      },
+    },
+  },
+}
